df9ad222b1d304aed9c5a93c3f5bf33b0e0158a4
Amita113 <135578813+Amita113@users.noreply.github.com>
2023-07-18 10:11:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Card from './card';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> This is a project on Rest API</h1>
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
